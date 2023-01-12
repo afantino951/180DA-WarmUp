5b72a39919a4739af94add4120e71c10075a0c0e
afantino951 <andrewfa@g.ucla.edu>
2023-01-12 15:28:02 -0800

    Task 3: Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    x = "ECE_180_DA_DB"
+    if x == "ECE_180_DA_DB":
+        print("You are living in 2023")
+    else: 
+        x += " - Best class ever"
+        print(x)
+
